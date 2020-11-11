5f3c5a8b586b3f970c1b4495dd6bd768879a8834
mazharmahmmood <mazhar.mahmmood@gmail.com>
2020-11-11 15:51:36 +0500

    hello.m file added

---
+++ hello.m
@@ -0,0 +1,4 @@
+n = input('Enter a number:');
+for sentence = 1:n  % have the indexing go from 1 to n, not reverse
+    fprintf('%d. Hello world!\n', sentence); % print the index, and a newline \n
+end
